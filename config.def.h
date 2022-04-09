0a5ece44e410a34ae07589f443f457f21c5933b2
Athul Dinesan <athuldinesan5@gmail.com>
2022-04-10 00:22:10 +0530

    dmenu: cleanup

--- config.def.h
+++ config.def.h
@@ -4,19 +4,20 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Fira Code:size=11:style:Medium"
+	"Fira Code:size=12:style:Medium"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#000000", "#e76f51" },
-	[SchemeOut] = { "#14213d", "#e76f51" },
+	[SchemeNorm] = { "#f8f8f2", "#282a36" },
+	[SchemeSel] = { "#f8f8f2", "#6272a4" },
+	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 22;
+static unsigned int lineheight = 25;
 static unsigned int min_lineheight = 8;
 
 /*
